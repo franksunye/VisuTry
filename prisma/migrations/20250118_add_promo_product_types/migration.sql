0593cdc6c48dcbda88040d193bf965f7242a8bb7
franksunye <franksunye@example.com>
2026-01-18 19:00:34 +0800

    feat: add promo ProductType enum values with Prisma migration

---
+++ prisma/migrations/20250118_add_promo_product_types/migration.sql
@@ -0,0 +1,4 @@
+-- Add new promo product types to ProductType enum
+ALTER TYPE "ProductType" ADD VALUE 'CREDITS_PACK_PROMO_60';
+ALTER TYPE "ProductType" ADD VALUE 'PREMIUM_MONTHLY_PROMO';
+ALTER TYPE "ProductType" ADD VALUE 'PREMIUM_YEARLY_PROMO';
