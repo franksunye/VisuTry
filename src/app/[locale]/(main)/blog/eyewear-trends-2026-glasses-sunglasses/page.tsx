1ab7cef36fd14d57e8d110dc3ee31d891807a636
Ye Sun <franksunye@hotmail.com>
2026-07-20 22:37:26 +0800

    fix: use supported briefcase icon export

--- src/app/[locale]/(main)/blog/eyewear-trends-2026-glasses-sunglasses/page.tsx
+++ src/app/[locale]/(main)/blog/eyewear-trends-2026-glasses-sunglasses/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {
   ArrowRight,
-  BriefcaseBusiness,
+  Briefcase as BriefcaseBusiness,
   CheckCircle2,
   Glasses,
   ShieldCheck,
