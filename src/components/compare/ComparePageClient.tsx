c70f869437661ec658359e8e78ee83f1362a50a1
Ye Sun <franksunye@hotmail.com>
2026-08-10 19:17:34 +0800

    refactor: unify mobile compare credit exhaustion UX

--- src/components/compare/ComparePageClient.tsx
+++ src/components/compare/ComparePageClient.tsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react'
 import { ReactNode } from 'react'
 import { AutoRefreshWrapper } from '@/components/payments/AutoRefreshWrapper'
 import { ConversionPaywallBoundary } from '@/components/payments/ConversionPaywallBoundary'
+import { CreditExhaustedSurface } from '@/components/payments/CreditExhaustedBar'
 import { FrameCompareInterface } from '@/components/compare/FrameCompareInterface'
 
 interface ComparePageClientProps {
@@ -29,7 +30,13 @@ export function ComparePageClient({ landing }: ComparePageClientProps) {
   return (
     <AutoRefreshWrapper>
       <ConversionPaywallBoundary source="frame_compare">
-        <FrameCompareInterface initialRemainingCredits={initialRemainingCredits} />
+        <CreditExhaustedSurface
+          kind="frame_compare"
+          availableCredits={initialRemainingCredits}
+          requiredCredits={4}
+        >
+          <FrameCompareInterface initialRemainingCredits={initialRemainingCredits} />
+        </CreditExhaustedSurface>
       </ConversionPaywallBoundary>
     </AutoRefreshWrapper>
   )
