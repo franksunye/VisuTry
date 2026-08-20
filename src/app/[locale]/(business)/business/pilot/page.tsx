a372e4433e30a4106d0569b7b92d960268142d0a
Ye Sun <franksunye@hotmail.com>
2026-08-20 14:25:17 +0800

    feat: add Business pilot page

---
+++ src/app/[locale]/(business)/business/pilot/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next'
+import { setRequestLocale } from 'next-intl/server'
+import { BusinessMarketingPage } from '@/components/business/BusinessMarketingPage'
+import { businessPages } from '@/config/business-site'
+
+export async function generateMetadata({ params }: { params: { locale: string } }): Promise<Metadata> {
+  const page = businessPages.pilot
+  return { title: page.metaTitle, description: page.metaDescription, alternates: { canonical: `https://www.visutry.com/${params.locale}${page.slug}` } }
+}
+
+export default function Page({ params }: { params: { locale: string } }) {
+  setRequestLocale(params.locale)
+  return <BusinessMarketingPage locale={params.locale} pageKey="pilot" />
+}
